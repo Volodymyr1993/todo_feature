71425da89ebdeb624936a80d345c8e93f73ac2c8
vtkachenko <vtkachenko@340basics.com>
2023-02-19 12:31:23 +0200

    Implement complete and exit button

--- gui.py
+++ gui.py
@@ -7,9 +7,14 @@ add_button = sq.Button("Add")
 list_box = sq.Listbox(values=functions.get_todos(), key="todos_list",
                       enable_events=True, size=[45, 10])
 edit_button = sq.Button("Edit")
+complete_button = sq.Button("Complete")
+exit_button = sq.Button("Exit")
 
 window = sq.Window("My To-Do App",
-                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
+                   layout=[[label],
+                           [input_box, add_button],
+                           [list_box, edit_button, complete_button],
+                           [exit_button]],
                    font=("Helvetica", 20)
                    )
 
@@ -34,6 +39,15 @@ while True:
             todos[index] = new_todo
             functions.write_todos(todos)
             window['todos_list'].update(values=todos)
+        case "Complete":
+            todo_to_complete = values["todos_list"][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window["todos_list"].update(values=todos)
+            window["todo"].update(value="")
+        case "Exit":
+            break
         case "todos_list":
             window["todo"].update(value=values['todos_list'][0])
         case sq.WIN_CLOSED:
